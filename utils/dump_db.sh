f476d74a20b7bf27276c853a0c92dad9ef946094
Robin Richardson <raarichardson@gmail.com>
2021-09-09 14:42:07 +0200

    Make dump_db.sh handle situation where db is not running

--- utils/dump_db.sh
+++ utils/dump_db.sh
@@ -3,6 +3,10 @@
 
 timestamp=$(date +%Y%m%d-%H%M%S)
 dump_fname=perfectfitdb-${timestamp}.dump
+
+# First check if the db is actually running by asking to dump the schema only
+docker compose exec -u root db pg_dump -s perfectfit > /dev/null || { echo "dump_db.sh: Could not find running database." ; exit 1 ; }
+
 echo "Dumping running db to: ${dump_fname} ..."
-docker compose exec -u root db pg_dump -Fc perfectfit > ${dump_fname}
+docker compose exec -u root db pg_dump -Fc perfectfit > ${dump_fname} || { echo "Error occured when dumping db. Dump file is probably bad or empty." ; exit 1 ; }
 echo "...done"
