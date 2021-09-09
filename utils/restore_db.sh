0d5f717210c95236c19e4608c5d6fed265e30718
Robin Richardson <raarichardson@gmail.com>
2021-09-09 14:42:30 +0200

    Make restore_db.sh handle situation where db is not running

--- utils/restore_db.sh
+++ utils/restore_db.sh
@@ -8,5 +8,5 @@ fi
 dump_fname=$1
 
 echo "Restoring db state from: ${dump_fname} ..."
-docker compose exec -T -u root db pg_restore -c -d perfectfit < ${dump_fname}
+docker compose exec -T -u root db pg_restore -c -d perfectfit < ${dump_fname} || { echo "Error occured when trying to restore db from dump. Is db running?" ; exit 1 ; }
 echo "...done"
