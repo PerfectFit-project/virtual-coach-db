563e0c1ec320e32ecf56b4df6a4b10d532f830e8
Bouke Scheltinga <b.l.scheltinga@utwente.nl>
2021-10-21 13:06:25 +0200

    Create one-to-many mapping in database

--- dbschema/models.py
+++ dbschema/models.py
@@ -1,5 +1,6 @@
-from sqlalchemy import Column, Date, Integer, String
+from sqlalchemy import Column, Date, Integer, String, datetime, ForeignKey
 from sqlalchemy.ext.declarative import declarative_base
+from sqlalchemy.orm import relationship
 
 Base = declarative_base()
 
@@ -7,10 +8,20 @@ Base = declarative_base()
 class Users(Base):
     __tablename__ = 'users'
     id = Column(Integer, primary_key=True)
+    closed_user_answers = relationship('Closed_user_answers')
     nicedayuid = Column(Integer)
     firstname = Column(String)
     lastname = Column(String)
     location = Column(String)
     gender = Column(String)
     dob = Column(Date)
-    paevaluation = Column(Integer)  # Temporary, will be changed with issue #125
+
+
+class Closed_user_answers(Base):
+    __tablename__ = 'closed_user_answers'
+    id = Column(Integer, primary_key=True)
+    users_id = Column(Integer, ForeignKey('users.id'))
+    # parent = relationship("Users", back_populates="closed_user_answers")
+    value = Column(Integer)
+    question = Column(String)
+    datetime = Column(datetime)
