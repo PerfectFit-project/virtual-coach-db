0a9f8220ca93a8d288eb7853f1caf570534acb3b
Bouke Scheltinga <b.l.scheltinga@utwente.nl>
2021-11-11 14:45:56 +0100

    Update relation closed_user_answers

--- dbschema/models.py
+++ dbschema/models.py
@@ -8,7 +8,7 @@ Base = declarative_base()
 class Users(Base):
     __tablename__ = 'users'
     id = Column(Integer, primary_key=True)
-    closed_user_answers = relationship('closed_user_answers')
+    closed_user_answers = relationship('ClosedUserAnswers')
     nicedayuid = Column(Integer)
     firstname = Column(String)
     lastname = Column(String)
