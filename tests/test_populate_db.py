1489a33b65c73b9370a93b98e459f20d5eef4cb1
Sven van der Burg <s.vanderburg@esciencecenter.nl>
2022-01-03 15:20:25 +0100

    Add code for populating database and corresponding tests

---
+++ tests/test_populate_db.py
@@ -0,0 +1,18 @@
+from sqlalchemy.ext.declarative.clsregistry import _ModuleMarker
+
+from dbschema.models import Base
+from helper.populate_db import populate_db_with_test_data
+
+
+def test_populate_db_with_test_data(db_session):
+    """
+    Test populating the database with test data.
+    We enforce that for each Model some test data must be added.
+    """
+    populate_db_with_test_data(db_session)
+    # Go through all SQLAlchemy models
+    for name, klass in Base._decl_class_registry.items():
+        if isinstance(klass, _ModuleMarker):
+            continue
+        assert db_session.query(klass).count() > 0, \
+            f'Class {klass} does not have any rows populated with test data'
