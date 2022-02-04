df6d112587dc325ae8f1700fc3daf5edb3541994
Bouke Scheltinga <b.l.scheltinga@utwente.nl>
2022-02-04 14:10:38 +0100

    Rename id into nicedayuid to match new db schema

--- helper/populate_db.py
+++ helper/populate_db.py
@@ -10,10 +10,14 @@ def populate_db_with_test_data(session):
     """
     session.add_all([
         Users(dob=date(2019, 4, 13), firstname='Sven', gender='MALE', lastname='van der burg',
-              location='Damsko', id=38527),
+              location='Damsko', nicedayuid=38527),
         Users(dob=date(2018, 1, 2), firstname='Nele', gender='FEMALE', lastname='Albers',
-              location='Delft', id=40121),
-        ClosedUserAnswers(users_id=38527, value=3, question='paevaluation', datetime=datetime.now())
+              location='Delft', nicedayuid=40121),
+        ClosedUserAnswers(users_nicedayuid=38527, value=3, question='paevaluation', datetime=datetime.now()),
+        ClosedUserAnswers(users_nicedayuid=38527, value=5, question='paevaluation', datetime=datetime.now()),
+        ClosedUserAnswers(users_nicedayuid=38527, value=4, question='paevaluation', datetime=datetime.now()),
+        ClosedUserAnswers(users_nicedayuid=40121, value=2, question='paevaluation', datetime=datetime.now()),
+        ClosedUserAnswers(users_nicedayuid=40121, value=1, question='paevaluation', datetime=datetime.now()),
     ])
     session.commit()
 
