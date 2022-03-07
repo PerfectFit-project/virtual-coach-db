3af6765908e412b6a74bcca315b3d6d3bffbddd2
Sven van der Burg <s.vanderburg@esciencecenter.nl>
2022-03-07 10:51:26 +0100

    Upsert instead of insert for populate db

--- helper/populate_db.py
+++ helper/populate_db.py
@@ -1,3 +1,4 @@
+import logging
 from datetime import datetime, date
 
 from dbschema.models import Users, ClosedUserAnswers
@@ -6,22 +7,25 @@ from helper import get_db_session
 
 def populate_db_with_test_data(session):
     """
-    Populate the database with test data.
+    Populate the database with test data. Update data if it already exists.
     """
-    session.add_all([
+    objects = [
         Users(dob=date(2019, 4, 13), firstname='Sven', gender='MALE', lastname='van der burg',
               location='Damsko', nicedayuid=38527),
         Users(dob=date(2018, 1, 2), firstname='Nele', gender='FEMALE', lastname='Albers',
               location='Delft', nicedayuid=40121),
-        ClosedUserAnswers(users_nicedayuid=38527, value=3, question='paevaluation', datetime=datetime.now()),
-        ClosedUserAnswers(users_nicedayuid=38527, value=5, question='paevaluation', datetime=datetime.now()),
-        ClosedUserAnswers(users_nicedayuid=38527, value=4, question='paevaluation', datetime=datetime.now()),
-        ClosedUserAnswers(users_nicedayuid=40121, value=2, question='paevaluation', datetime=datetime.now()),
-        ClosedUserAnswers(users_nicedayuid=40121, value=1, question='paevaluation', datetime=datetime.now()),
-    ])
+        ClosedUserAnswers(id=1, users_nicedayuid=38527, value=3, question='paevaluation', datetime=datetime.now()),
+        ClosedUserAnswers(id=2, users_nicedayuid=38527, value=5, question='paevaluation', datetime=datetime.now()),
+        ClosedUserAnswers(id=3, users_nicedayuid=38527, value=4, question='paevaluation', datetime=datetime.now()),
+        ClosedUserAnswers(id=4, users_nicedayuid=40121, value=2, question='paevaluation', datetime=datetime.now()),
+        ClosedUserAnswers(id=5, users_nicedayuid=40121, value=1, question='paevaluation', datetime=datetime.now()),
+    ]
+    [session.merge(obj) for obj in objects]
     session.commit()
 
 
 if __name__ == '__main__':
-    session = get_db_session()
+    logging.getLogger().setLevel(logging.INFO)
+    session = get_db_session(db_host='localhost:5432')
     populate_db_with_test_data(session)
+    logging.info('Succesfully populated database with test data')
