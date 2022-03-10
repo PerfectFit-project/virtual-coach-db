0f4715ef647931ec6b51d9658bf02bb4f3463e62
Nele Albers <nalbers1@tudelft.net>
2022-03-10 08:57:55 +0100

    add intervention state data to populate db

--- helper/populate_db.py
+++ helper/populate_db.py
@@ -1,7 +1,7 @@
 import logging
 from datetime import datetime, date
 
-from dbschema.models import Users, ClosedUserAnswers
+from dbschema.models import Users, ClosedUserAnswers, UserInterventionState
 from helper import get_db_session
 
 
@@ -19,6 +19,7 @@ def populate_db_with_test_data(session):
         ClosedUserAnswers(id=3, users_nicedayuid=38527, value=4, question='paevaluation', datetime=datetime.now()),
         ClosedUserAnswers(id=4, users_nicedayuid=40121, value=2, question='paevaluation', datetime=datetime.now()),
         ClosedUserAnswers(id=5, users_nicedayuid=40121, value=1, question='paevaluation', datetime=datetime.now()),
+        UserInterventionState(id=1, users_nicedayuid=40121, futureselfdialogdatetime=datetime.now(), futureselfdialogpart="step_1"),
     ]
     [session.merge(obj) for obj in objects]
     session.commit()
