88ea602d80a0b5f6be6ad9e8390b9280412a18ca
Nele Albers <nalbers1@tudelft.net>
2022-03-14 12:09:26 +0100

    need integer for populate db now too

--- helper/populate_db.py
+++ helper/populate_db.py
@@ -19,7 +19,7 @@ def populate_db_with_test_data(session):
         ClosedUserAnswers(id=3, users_nicedayuid=38527, value=4, question='paevaluation', datetime=datetime.now()),
         ClosedUserAnswers(id=4, users_nicedayuid=40121, value=2, question='paevaluation', datetime=datetime.now()),
         ClosedUserAnswers(id=5, users_nicedayuid=40121, value=1, question='paevaluation', datetime=datetime.now()),
-        UserInterventionState(users_nicedayuid=40121, intervention_component="future_self_dialog", last_time=datetime.now(), last_part="step_1"),
+        UserInterventionState(users_nicedayuid=40121, intervention_component="future_self_dialog", last_time=datetime.now(), last_part=1),
     ]
     [session.merge(obj) for obj in objects]
     session.commit()
