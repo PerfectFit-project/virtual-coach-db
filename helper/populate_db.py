1697515acbe48ad7fea0a1077e3ba0485ca9b0a5
Bouke Scheltinga <b.l.scheltinga@utwente.nl>
2022-03-14 16:17:12 +0100

    Change order of filling db

--- helper/populate_db.py
+++ helper/populate_db.py
@@ -1,7 +1,7 @@
 import logging
 from datetime import datetime, date
 
-from dbschema.models import Users, ClosedUserAnswers, UserInterventionState, DialogQuestions
+from dbschema.models import Users, ClosedUserAnswers, UserInterventionState, DialogQuestions, DialogAnswers
 from helper import get_db_session
 
 
@@ -9,6 +9,15 @@ def populate_db_with_test_data(session):
     """
     Populate the database with test data. Update data if it already exists.
     """
+    # Fill question table
+    objects_questions = [
+        DialogQuestions(question_id=1, question_description='future self dialog - smoker words'),
+        DialogQuestions(question_id=2, question_description='future self dialog - smoker why'),
+        DialogQuestions(question_id=3, question_description='future self dialog - mover words'),
+        DialogQuestions(question_id=4, question_description='future self dialog - mover why')
+    ]
+    [session.merge(obj) for obj in objects_questions]
+
     objects = [
         Users(dob=date(2019, 4, 13), firstname='Sven', gender='MALE', lastname='van der burg',
               location='Damsko', nicedayuid=38527),
@@ -22,26 +31,16 @@ def populate_db_with_test_data(session):
         ClosedUserAnswers(id=4, users_nicedayuid=40121, value=2, question='paevaluation', datetime=datetime.now()),
         ClosedUserAnswers(id=5, users_nicedayuid=40121, value=1, question='paevaluation', datetime=datetime.now()),
         DialogAnswers(answer_id=1, users_nicedayuid=38527, answer='lekker stoer eng', question_id=1,
-                      datetime=datetime.now())
-        UserInterventionState(users_nicedayuid=40121, intervention_component="future_self_dialog", last_time=datetime.now(), last_part=1),
-
+                      datetime=datetime.now()),
+        UserInterventionState(users_nicedayuid=40121, intervention_component="future_self_dialog", last_time=datetime.now(), last_part=1)
     ]
     [session.merge(obj) for obj in objects]
-
-    # Fill question table
-    objects_questions = [
-        DialogQuestions(question_id=1, question_description='future self dialog - smoker words'),
-        DialogQuestions(question_id=2, question_description='future self dialog - smoker why'),
-        DialogQuestions(question_id=3, question_description='future self dialog - mover words'),
-        DialogQuestions(question_id=4, question_description='future self dialog - mover why')
-    ]
-    [session.merge(obj) for obj in objects_questions]
 
     session.commit()
 
 
 if __name__ == '__main__':
     logging.getLogger().setLevel(logging.INFO)
-    session = get_db_session()
+    session = get_db_session(db_host='localhost:5432')
     populate_db_with_test_data(session)
     logging.info('Succesfully populated database with test data')
