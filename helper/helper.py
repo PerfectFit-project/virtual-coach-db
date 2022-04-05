8ed1a4be29d24030536fe2f2305fa28f59104781
Robin Richardson <raarichardson@gmail.com>
2022-04-05 16:09:23 +0200

    Convert heroku db urls to sqlalchemy acceptable form

--- helper/helper.py
+++ helper/helper.py
@@ -3,11 +3,27 @@ import sys
 from sqlalchemy import MetaData, create_engine
 from sqlalchemy.orm import sessionmaker
 
-DB_URL = 'postgresql://root:root@db:5432/perfectfit'
+DB_URL_DEFAULT = 'postgresql://root:root@db:5432/perfectfit'
 
+def santize_db_url(db_url):
+    """
+    SQLAlchemy cannot handle the "postgres" prefix
+    (see https://github.com/sqlalchemy/sqlalchemy/issues/6083), but Heroku still populates
+    DATABASE_URL with that protocol designator. For such URLs we need to modify the string
+    from "postgres" -> "postgresql".
 
-def get_db_session(db_url=DB_URL):
-    engine = create_engine(db_url)
+    We are doing this string replacement because the config var DATABASE_URL provided by
+    Heroku may change.
+    """
+    if db_url.startswith("postgres://"):
+        db_url = db_url.replace("postgres://", "postgresql://", 1)
+
+    return db_url
+
+def get_db_session(db_url=DB_URL_DEFAULT):
+
+    sanitized_db_url = santize_db_url(db_url)
+    engine = create_engine(sanitized_db_url)
     meta = MetaData()
     meta.reflect(bind=engine)
 
