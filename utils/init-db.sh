4cd782316aa85fd35cbf317ecb4d6c244624d370
Bouke Scheltinga <b.l.scheltinga@utwente.nl>
2022-05-11 16:03:21 +0200

    Make sure error code is returned

--- utils/init-db.sh
+++ utils/init-db.sh
@@ -1,7 +1,7 @@
 #! /bin/bash
 alembic upgrade head
 if [ ${ENVIRONMENT} -eq "dev" ]; then
-  python3 ../helper/populate_db.py
+  python3 ../helper/populate_db.py || exit 1
 fi
 
 # Keep container running
