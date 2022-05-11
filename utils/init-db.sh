ad9883699c044809c69888db92afe05841775750
Bouke Scheltinga <b.l.scheltinga@utwente.nl>
2022-05-11 16:11:20 +0200

    Change string comparison

--- utils/init-db.sh
+++ utils/init-db.sh
@@ -1,6 +1,6 @@
 #! /bin/bash
 alembic upgrade head
-if [ ${ENVIRONMENT} -eq "dev" ]; then
+if [ ${ENVIRONMENT} = "dev" ]; then
   python3 ../helper/populate_db.py || exit 1
   echo "Successfully applied populate_db"
 fi
