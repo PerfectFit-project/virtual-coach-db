59779f3457a0312d4f50045212e0abc37d11339d
Bouke Scheltinga <b.l.scheltinga@utwente.nl>
2022-12-01 14:18:18 +0100

    Add dialog questions as enum

--- helper/definitions.py
+++ helper/definitions.py
@@ -37,3 +37,32 @@ class ExecutionInterventionComponentsTriggers(str, Enum):
     GENERAL_ACTIVITY = 'EXTERNAL_trigger_general_activity'
     WEEKLY_REFLECTION = 'EXTERNAL_weekly_reflection'
     DAILY_REFLECTION = 'EXTERNAL_daily_reflection'
+
+
+class DialogQuestionsEnum(Enum):
+    FUTURE_SELF_SMOKER_WORDS = 1  # Which three words suits you as smoker?
+    FUTURE_SELF_SMOKER_WHY = 2  # Why did you pick these words for smoking?
+    FUTURE_SELF_MOVER_WORDS = 3  # Which three words suits you as exerciser?
+    FUTURE_SELF_MOVER_WHY = 4  # Why did you pick these words for exercising?
+    FUTURE_SELF_I_SEE_MYSELF_AS_SMOKER = 5  # I see myself as smoker, non-smoker or quitter
+    FUTURE_SELF_I_SEE_MYSELF_AS_MOVER = 6  # I see myself as active, bit active or not active
+    RELAPSE_CRAVING_WHAT_DOING = 7
+    RELAPSE_CRAVING_HOW_FEEL = 8
+    RELAPSE_CRAVING_WITH_WHOM = 9
+    RELAPSE_CRAVING_HAPPENED_SPECIAL = 10
+    RELAPSE_LAPSE_TYPE_SMOKE = 11
+    RELAPSE_LAPSE_NUMBER_CIGARETTES = 12
+    RELAPSE_LAPSE_WHAT_DOING = 13
+    RELAPSE_LAPSE_HOW_FEEL = 14
+    RELAPSE_LAPSE_WITH_WHOM = 15
+    RELAPSE_LAPSE_HAPPENED_SPECIAL = 16
+    RELAPSE_RELAPSE_TYPE_SMOKE = 17
+    RELAPSE_RELAPSE_NUMBER_CIGARETTES = 18
+    RELAPSE_RELAPSE_WHAT_DOING = 19
+    RELAPSE_RELAPSE_HOW_FEEL = 20
+    RELAPSE_RELAPSE_WITH_WHOM = 21
+    RELAPSE_RELAPSE_HAPPENED_SPECIAL = 22
+    RELAPSE_PA_TOGETHER = 23
+    RELAPSE_PA_WHY_FAIL = 24
+    RELAPSE_PA_DOING_TODAY = 25
+    RELAPSE_PA_HAPPENED_SPECIAL = 26
